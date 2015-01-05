a02c888e7aeaea85b099e28b5bcf0e115cfb3a47
EpicKitty <EpicnessTwo@users.noreply.github.com>
2015-01-05 12:23:55 +0000

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,21 @@
+<?php
+// TinyBot - Written by EpicKitty (EpicnessTwo)
+
+//Vars
+
+$config['serverip'] = 'irc.esper.net'; //Server address
+$config['port'] = 6667; //Server port
+$config['name'] = 'TinyBot'; //Bot's name
+$config['user'] = 'TinyBot'; //Bot's user
+$config['nick'] = 'TinyBot'; //Bot's nick
+$config['prefix'] = '~'; //Command prefix
+
+$config['usebnc'] = true; //Will this be on a bnc?
+$config['serverid'] = 'serv1'; //To identify the server
+$config['bncpassword'] = 'enterpasshere'; //Password for bnc or server
+
+//Main
+
+//To be continued
+
+?>
